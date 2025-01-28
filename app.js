db933cf7daac9ee93a264d931c3895164207e6b0
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-01-28 19:18:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
 import { app } from "./FirebaseConfig.js";
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 const auth = getAuth(app); 
 
-
-document.querySelector("#signInButton").addEventListener("click",  () => {
+document.querySelector("#signInButton").addEventListener("click",  async () => {
   const email = document.querySelector("#email").value; 
   const password = document.querySelector("#password").value; 
 
-  signInWithEmailAndPassword(auth, email, password)
+  await signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log("User signed in:", user);
       alert("Successfully Signed In");
+      // local storage check
+      if (!localStorage.getItem("userData")) {
+        localStorage.setItem(
+          "userData",
+          JSON.stringify({
+            email: email,
+          })
+        );
+      }
+
       window.location.replace("dashboard.html");
     })
     .catch((error) => {
       console.error("Sign-in error:", error.message);
-      alert("Error: " + error.message);
-    });
-});
-
-document.querySelector("#signUpButton").addEventListener("click", () => {
-  const email = document.querySelector("#email").value; 
-  const password = document.querySelector("#password").value; 
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("User signed up:", user);
-      alert("Successfully Signed Up");
-      window.location.replace("index.html");
-    })
-    .catch((error) => {
-      console.error("Sign-up error:", error.message);
       alert("Error: " + error.message);
     });
 });
