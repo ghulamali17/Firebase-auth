cbcd5a5b17ac8c1ed97b54f9dcb5d3cc2ab97b89
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-02-11 11:18:14 +0500

    Update FirebaseConfig.js

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,13 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqrAAC7NV4Jff_TwQ7ZppumTRIlFKYlnc",
-  authDomain: "mad-project-9458c.firebaseapp.com",
-  projectId: "mad-project-9458c",
-  storageBucket: "mad-project-9458c.appspot.com",  // Fixed incorrect storage bucket domain
-  messagingSenderId: "544444484859",
-  appId: "1:544444484859:web:7b512e0a0c934d3426f45e"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+
+const FirebaseConfig = {
+  apiKey: "AIzaSyA4Syy5HnBB_8gLQ5g53zYjOpAYaChkTEY",
+  authDomain: "form-5437b.firebaseapp.com",
+  databaseURL: "https://form-5437b-default-rtdb.firebaseio.com",
+  projectId: "form-5437b",
+  storageBucket: "form-5437b.firebasestorage.app",
+  messagingSenderId: "1066721287085",
+  appId: "1:1066721287085:web:4cfdfd64352cd91e091f24",
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(FirebaseConfig);
